2228d87bc60e6e275a5b86809554c628c51133dc
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2021-08-07 17:04:35 +0100

    Update and rename memberBoost.js to guildMemberUpdate.js

---
+++ handlers/guildMemberUpdate.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+  name: `guildMemberUpdate`,
+  run: async (client, index, oldmember, newmember) {
+    
+    if(!oldmember.premium && newmember.premium) {
+      const amount = newmember.guild.premiumSubscriptionCount - oldmember.guild.premiumSubscriptionCount;
+      index.emit(`nitroBoostAdd`, (newmember, amount);;
+    }
+  }
+}
