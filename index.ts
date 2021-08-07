9e4465e5dae531ee49d0a7d475dac66dda072f06
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2021-08-07 16:56:23 +0100

    build(index.ts): Created Event Handler(s)

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 const EventEmitter = require('events');
+const { Client } = require('discord.js');
 
 module.exports = class NitroEvents extends EventEmitter {
-  constructor(client) {
+  constructor(private client) {
     super();
     this.client = client;
     
     if(!client.user) throw new Error(`Discord.Client is invalid.`);
   }
   
-  private emit
+  async emit(eventName: string, options) {
+    this.emit(eventName, options);
+  }
+  
+  async on(eventName: string, options) {
+    this.on(eventName, options);
+  }
 }
